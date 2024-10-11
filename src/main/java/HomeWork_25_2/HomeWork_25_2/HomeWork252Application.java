375e0318b323b491545606a8d8435bbd4065b6e8
Dolgorykij <dolgo-rykij@mail.ru>
2024-10-11 15:42:01 +0300

    init

---
+++ src/main/java/HomeWork_25_2/HomeWork_25_2/HomeWork252Application.java
@@ -0,0 +1,13 @@
+package HomeWork_25_2.HomeWork_25_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork252Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork252Application.class, args);
+	}
+
+}
---
+++ src/test/java/HomeWork_25_2/HomeWork_25_2/HomeWork252ApplicationTests.java
@@ -0,0 +1,13 @@
+package HomeWork_25_2.HomeWork_25_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork252ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
